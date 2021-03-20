87880bf289b12861b7bfc1a32b1a3f6787f13357
Johnathan <johnathanboyd5@gmail.com>
2021-03-19 22:32:50 -0500

    added post into the router

--- server/modules/routes/taskList_route.js
+++ server/modules/routes/taskList_route.js
@@ -3,4 +3,27 @@ const express = require( 'express' );
 const router = express.Router();
 const pool = require( '../pool');
 
+// routes with LOGIC
+
+// post here
+router.post('/', (req, res)=>{
+    console.log( 'taskList_route:', req.body );
+    // add this item to the DB 'tasks'
+    // create query string using SQL syntax!!
+    let queryString = `INSERT INTO "tasks" ( title, description ) VALUES ($1, $2)`;
+    // ask pool to run query
+    pool.query( queryString, [req.body.title, req.body.description] ).then (( results )=>{
+        // if successful send 200
+        res.sendStatus( 200 );
+    }).catch ( err )=>{
+        console.log( err );
+        res.sendStatus( 500 );
+    }
+}) // end POST
+
+
+
+
+
 module.exports = router
+
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -13,4 +13,15 @@ function addTask(){
         title: $( '#taskIn').val(),
         description: $('#descriptionIn').val()
     }
-}
+    // send object to server via ajax POST method
+    $.ajax({
+        method: 'POST',
+        url: '/tasks',
+        data: objectToSend
+    }).then( function( response ){
+        console.log( 'back from POST with:', response );
+    }).catch( function ( err ){
+        alert( 'error adding item to db');
+        console.log( err );
+    })// end ajax GOTO taskList_route
+} // end add Task 
