060f2f22bdeca5e308deb427c6ba6720dbd7f62a
Johnathan <johnathanboyd5@gmail.com>
2021-03-19 19:27:15 -0500

    added a template for my sql database

---
+++ database.sql
@@ -0,0 +1,10 @@
+-- db name tasks ---
+
+CREATE TABLE "tasks"(
+    "id" serial primary key,
+    "title" varchar(50),
+    "description" varchar(250)
+);
+
+INSERT INTO "tasks" ( title, description ) VAlUES ();
+SELECT * from "tasks";
