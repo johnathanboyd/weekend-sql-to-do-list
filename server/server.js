799f446733dbeb329840b7cb2e1e4dd174cbf3c0
Johnathan <johnathanboyd5@gmail.com>
2021-03-22 16:25:15 -0500

    initial commit: added things to heroku

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use( bodyParser.urlencoded( { extended: true } ) );
 app.use( '/tasks', taskList) ;
 
 // globals
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // spin up server
 app.listen( port, ()=>{
